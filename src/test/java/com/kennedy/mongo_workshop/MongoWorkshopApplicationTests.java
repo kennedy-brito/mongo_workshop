345940174fbed4d8005dbbe2c5321f0b4b2019d7
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-04 14:20:11 -0300

    Created Project

---
+++ src/test/java/com/kennedy/mongo_workshop/MongoWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennedy.mongo_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
